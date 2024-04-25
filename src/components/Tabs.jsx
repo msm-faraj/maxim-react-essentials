ae3c5fc2a97b9a1ddcded22d8ce144ed7a8fd7c3
msm-faraj <farajzademeysam@gmail.com>
2024-04-25 11:45:10 +0200

    done

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,7 +1,8 @@
-export default function Tabs({ children, buttons }) {
+export default function Tabs({ children, buttons, buttonsContainer = "menu" }) {
+  const ButtonsContainer = buttonsContainer; // :) like
   return (
     <>
-      <menu>{buttons}</menu>
+      <ButtonsContainer>{buttons}</ButtonsContainer>
       {children}
     </>
   );
